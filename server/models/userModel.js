86b31b04064d8d36addba07c9a53718bb251d865
bokim2 <bokim.ce@gmail.com>
2022-11-22 12:03:02 -0800

    database connected

---
+++ server/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// const userSchema = new Schema({
+//     userName: { type: String },
+//     userPassword : { type: String },
+//     orders: [{
+//         orderName: { type: String },
+//         items: [{
+            
+//         }]
+//     }]
+// })
+
+// const userSchema = new Schema ([
+//     orderName: String,
+//     items: [{strings}],
+//     total: String,
+    
+// ])
+
+// id
+// title
+// price
+// description
+// category
+// image
+//rating {
+//     rate:
+//     count:
+// }
+
