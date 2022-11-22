ed8117def10b8031517891c27205559bb8e17b3d
AndyW827 <andy.wong.km27@gmail.com>
2022-11-22 10:18:54 -0800

    fetchAPI completed

--- client/components/displayCard.jsx
+++ client/components/displayCard.jsx
@@ -5,22 +5,32 @@ import Cards from './Cards.jsx';
 
 //example data includes the item name, price, and image
 const DisplayCard = (props) => {
-  const {items} = props;
-  
-// const result = data.productItems.filter((item) => item.category === 'Produce');
-// console.log(result);
-  
-  return (
-    <section className='itemFlex'>
-      {items.map((item) => {
-        return (
-          <div key={item._id}>
-            <Cards  id={item._id} name={item.name} price={item.price} image={item.image} />
-          </div>
-        );
-      })}
-    </section>
-  );
+	// const { items } = props;
+
+	// const result = data.productItems.filter((item) => item.category === 'Produce');
+	// console.log(result);
+
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		fetch('https://fakestoreapi.com/products/')
+			.then((res) => res.json())
+			.then((data) => {
+				setItems(data);
+			});
+	}, []);
+
+	return (
+		<section className="itemFlex">
+			{items.map((item) => {
+				return (
+					<div key={item._id}>
+						<Cards id={item._id} name={item.name} price={item.price} image={item.image} />
+					</div>
+				);
+			})}
+		</section>
+	);
 };
 
 export default DisplayCard;
