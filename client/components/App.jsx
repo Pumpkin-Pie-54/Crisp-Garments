8baa69848a4a373ebde20e78303c02c0540c05af
bokim2 <bokim.ce@gmail.com>
2022-11-23 15:04:49 -0800

    nav working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,23 +2,24 @@ import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
 import React, { useState } from 'react';
 import Navbar from "./Navbar";
 import DisplayCards from "./DisplayCards.jsx";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
+import Cart from "./pages/Cart.js";
+import Login from "./pages/Login.js";
 
 import { TotalCartContextProvider } from "../context/TotalCartContext.jsx";
 
 export default function App() {
   const [category, setCategory] = useState('');
-
+  
+      // <div className="container">
+      //   <DisplayCards category={category} setCategory={setCategory} />
+      // </div>
   return (
     <TotalCartContextProvider>
       <Navbar category={category} setCategory={setCategory} />
-      <div className="container">
-        <DisplayCards category={category} setCategory={setCategory} />
-      </div>
+
 
       <Routes>
-
+        <Route path='/' element={<DisplayCards category={category} setCategory={setCategory}/>}></Route>
         <Route path='/cart' element={<Cart />}></Route>
         <Route path='/login' element={<Login />}></Route>
         
